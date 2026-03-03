d565bcec05a112bd54fa306f4fa3bb3f87f96f61
Momentum <bruhmomento840@gmail.com>
2026-03-02 23:33:32 -0600

    first go
    oh boy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # prescript generator
  creates prescripts in a similar fashion to the index pager from limbus company/library of ruina i forgot which
+
+this was made for funsies and also because i was bored
+do whatever
--- prescript.py
+++ prescript.py
@@ -1,5 +1,6 @@
 import json
 import random
+import string
 import time
 import os
 
@@ -18,8 +19,56 @@ def generatePrescript():
     act2 = random.choice(words["secondAct"])
     closing = random.choice(words["closing"])
     
-    prescript = f"{condition} {act1} and find the {numStr} {adj} {noun} you see and {act2}. {closing}"
+    prescript = f"{condition}, {act1} and find the {numStr} {adj} {noun} you see and {act2}. {closing}"
     
     return prescript.strip()
 
-print (generatePrescript())
+def typewriting(text, speed=0.05):
+    for char in text:
+        print(char, end='', flush=False) # set flush to True for instant output
+        time.sleep(speed)
+    print()
+
+# cool shit zone
+def charSpamShow(targetText, speed=0.05):
+    spamChar = string.ascii_letters + string.punctuation + string.digits
+    currentText = [random.choice(spamChar) for _ in range(len(targetText))]
+    
+    for i in range(len(targetText)):
+        for j in range(i, len(targetText)):
+            currentText[j] = random.choice(spamChar)
+            
+        currentText[i] = targetText[i]
+        
+        print('\r' + "".join(currentText), end='', flush=True) 
+        time.sleep(speed)
+        
+    print()
+    
+def charSpamHide(targetText, speed=0.05):
+    spamChar = string.ascii_letters + string.punctuation + string.digits
+    currentText = list(targetText)
+    
+    for i in range(len(targetText)):
+        for j in range(i + 1):
+            currentText[j] = random.choice(spamChar)
+            
+        print('\r' + "".join(currentText), end='', flush=True)
+        time.sleep(speed)
+        
+    time.sleep(0.5)
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    
+    
+    for char in "_CLEAR._":
+        print(char, end='', flush=True) 
+        time.sleep(0.15)
+    print("\n")
+    
+# --- OUTPUT ---
+finalPrescript = generatePrescript()
+charSpamShow(finalPrescript, speed=0.02)
+time.sleep(3)
+charSpamHide(finalPrescript, speed=0.02)
